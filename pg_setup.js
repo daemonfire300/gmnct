6a2f6ea99911be603ee102de833cdd923d5942d4
Julius Foitzik <j@accountr.eu>
2012-10-13 12:47:23 -0400

    add pg db setup

--- pg_setup.js
+++ pg_setup.js
@@ -1,7 +1,7 @@
 var pg = require('pg').native,
     connectionString = process.env.DATABASE_URL,
     client, query;
-
+console.log("connecting to: "+connectionString);
 client = new pg.Client(connectionString);
 client.connect();
 query = client.query('CREATE TABLE users (username text, email text)');
