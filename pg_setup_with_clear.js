ad1e0bd4db379d7098337c387c5df3786091046d
Julius Foitzik <j@accountr.eu>
2012-10-17 16:02:59 -0400

    changed database layout to support UNIQUES

--- pg_setup_with_clear.js
+++ pg_setup_with_clear.js
@@ -5,7 +5,9 @@ console.log("connecting to: "+connectionString);
 client = new pg.Client(connectionString);
 client.connect();
 client.query('DROP TABLE IF EXISTS users');
-query = client.query('CREATE TABLE users (id SERIAL PRIMARY KEY, username varchar(64), email varchar(255), password varchar(255), registration_opt_in boolean DEFAULT false)');
+query = client.query(
+    'CREATE TABLE users (id SERIAL PRIMARY KEY, username varchar(64) UNIQUE, email varchar(255) UNIQUE, password varchar(255), registration_opt_in boolean DEFAULT false)'
+    );
 query.on('end', function() {
     client.end();
 });
