7b8b1beba65be93d1022e5bd270521dd2d3e2df9
Julius Foitzik <j@accountr.eu>
2012-10-18 16:06:23 -0400

    ...

--- pg_setup_with_clear.js
+++ pg_setup_with_clear.js
@@ -4,7 +4,10 @@ var pg = require('pg').native,
 console.log("connecting to: " + connectionString);
 client = new pg.Client(connectionString);
 client.connect();
-var query = client.query('DROP TABLE IF EXISTS users');
+var query = client.query('DROP TABLE IF EXISTS lobbies');
+query = client.query('DROP TABLE IF EXISTS games');
+query = client.query('DROP TABLE IF EXISTS users');
+
 query.on("end", function(){
     var query = client.query('CREATE TABLE users (id SERIAL PRIMARY KEY, username varchar(64) UNIQUE, email varchar(255) UNIQUE, password varchar(255), registration_opt_in boolean DEFAULT false)');
     query.on("end", function(){
