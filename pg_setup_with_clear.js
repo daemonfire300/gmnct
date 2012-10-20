bfaa47a056e23126925366b471e65ecce52ee6fa
Julius Foitzik <j@accountr.eu>
2012-10-20 12:04:07 -0400

    ...

--- pg_setup_with_clear.js
+++ pg_setup_with_clear.js
@@ -10,6 +10,7 @@ client.query('DROP TABLE IF EXISTS users');
 client.query('DROP TABLE IF EXISTS lobby_userlist');
 
 
+
 client.query('CREATE TABLE users (id SERIAL PRIMARY KEY, username varchar(64) UNIQUE, email varchar(255) UNIQUE, password varchar(255), registration_opt_in boolean DEFAULT false)');
 client.query('CREATE TABLE games (id SERIAL PRIMARY KEY, name varchar(128) UNIQUE, category varchar(255) )');
 client.query('CREATE TABLE lobbies (id SERIAL PRIMARY KEY, name varchar(128) UNIQUE, owner integer REFERENCES users(id), game integer REFERENCES games(id) )');
