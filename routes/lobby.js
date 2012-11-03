5c64262358d8a36d7c637e7199f1a657f965cfcd
Julius Foitzik <j@accountr.eu>
2012-11-03 15:33:51 -0400

    ...

--- routes/lobby.js
+++ routes/lobby.js
@@ -1,5 +1,5 @@
 var async = require("async");
-var JSONResponse = require("/lib/JSONResponse");
+var JSONResponse = require("./lib/JSONResponse");
 
 module.exports = function(client, check, sanitize) {
     return {
